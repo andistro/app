787c2b662b2d71285044573f38e6ecdaa0146266
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-05-25 20:03:34 -0300

    v0.0.67.4.79
    * Algumas novas mudanças para teste de código

--- config/locale/locale_pt-BR.sh
+++ config/locale/locale_pt-BR.sh
@@ -3,7 +3,7 @@
 source "/usr/local/bin/fixed_variables.sh"
 
 error_code="LG002br"
-show_progress_dialog "steps" "${label_system_language}" 8 \
+show_progress_dialog "steps-one-label" "${label_system_language}" 8 \
     "apt-get update" \
     "apt-get install locales -y" \
     "apt-get install language-pack-pt-base -y" \
@@ -11,7 +11,7 @@ show_progress_dialog "steps" "${label_system_language}" 8 \
     "locale-gen" \
     "echo 'export LC_ALL=pt_BR.UTF-8' >> ~/.bashrc" \
     "echo 'export LANG=pt_BR.UTF-8' >> ~/.bashrc" \
-    "echo 'export LANGUAGE=pt_BR.UTF-8' >> ~/.bashrc" \
+    "echo 'export LANGUAGE=pt_BR.UTF-8' >> ~/.bashrc"
 
 exit_erro
 ## Exportar os comandos de configuração de idioma para ~/.bashrc
