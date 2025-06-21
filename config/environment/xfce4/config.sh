6263dea27fd01a8a0f6c1512143aeb9dd0a99172
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-06-21 00:58:24 -0300

    v0.0.67.5.19
    *Melhorias sutis no código
    * Melhoria no xódigo de instalação do XFCE4

--- config/environment/xfce4/config.sh
+++ config/environment/xfce4/config.sh
@@ -19,61 +19,18 @@ show_progress_dialog steps-one-label "${label_install_environment_gui}" 18 \
 export PULSE_SERVER=127.0.0.1
 export LANG
 [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r \$HOME/.Xresources ] && xrdb \$HOME/.Xresources
-echo \$\$ > /tmp/xsession.pid
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+echo $$ > /tmp/xsession.pid
 dbus-launch --exit-with-session /usr/bin/startxfce4
 EOF
 "' \
      'chmod +x ~/.vnc/xstartup' \
      "echo 'export DISPLAY=":1"' >> /etc/profile" \
      "wget --tries=20 '${extralink}/config/environment/xfce4/start-environment.sh'" \
-     "[ -f ~/start-environment.sh ] && chmod +x ~/start-environment.sh" \
-     "sudo dpkg --configure -a" \
-     "sudo apt --fix-broken install -y" 
+     '[ -f ~/start-environment.sh ] && chmod +x ~/start-environment.sh' \
+     'sudo dpkg --configure -a' \
+     'sudo apt --fix-broken install -y'
 sleep 2
-
-
-
-
-
-
-
-show_progress_dialog steps-one-label "${label_install_environment_gui}" 17 \
-     'sudo apt-get install xfce4 --no-install-recommends -y' \
-     'sudo apt-get install xfce4-goodies --no-install-recommends -y' \
-     'sudo apt-get install xfce4-terminal --no-install-recommends -y' \
-     'sudo apt-get install xfce4-panel-profiles --no-install-recommends -y' \
-     'sudo apt-get install exo-utils --no-install-recommends -y' \
-     'sudo apt-get install tigervnc-standalone-server --no-install-recommends -y' \
-     'sudo apt-get install tigervnc-common --no-install-recommends -y' \
-     'sudo apt-get install tigervnc-tools --no-install-recommends -y' \
-     'sudo apt-get install dbus-x11 --no-install-recommends -y' \
-     'sudo apt install python3-gi -y' \
-     'sudo apt install python3 -y' \
-     'bash -c "cat > $HOME/.vnc/xstartup <<- EOM
-#!/bin/bash
-export PULSE_SERVER=127.0.0.1
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r \$HOME/.Xresources ] && xrdb \$HOME/.Xresources
-echo \$\$ > /tmp/xsession.pid
-dbus-launch --exit-with-session /usr/bin/startxfce4
-EOM
-"' \
-     'chmod +x ~/.vnc/xstartup' \
-     "echo 'export DISPLAY=":1"' >> /etc/profile" \
-     "wget --tries=20 '${extralink}/config/environment/lxde/start-environment.sh'" \
-     "[ -f ~/start-environment.sh ] && chmod +x ~/start-environment.sh" \
-     "sudo dpkg --configure -a" \
-     "sudo apt --fix-broken install -y" 
-
-
-
-
-
-
-
-
 
 vncpasswd
 
