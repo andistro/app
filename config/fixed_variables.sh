d0fd23c0163710558a90fc05fdc0c7fb5b55dcc1
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-07-03 21:10:02 -0300

    v0.0.67.5.25
    *Alteração do link de downloads

--- config/fixed_variables.sh
+++ config/fixed_variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "v0.0.67.5.26"
+echo "v0.0.67.5.25"
 export extralink="https://raw.githubusercontent.com/andistro/app/alpha"
 export NEWT_COLORS="window=,white border=black,white title=black,white textbox=black,white button=white,blue"
 
