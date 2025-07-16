d368e026638d80d5d705d47860c156fe9d97afe9
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-07-16 01:46:41 -0300

    v0.0.67.5.34
    * Instalador do alpine

--- distros/alpine.sh
+++ distros/alpine.sh
@@ -3,3 +3,27 @@ source "$PREFIX/bin/andistro_files/global_var_fun.sh"
 bin="start-alpine.sh"
 codinome="edge"
 folder="alpine-edge"
+
+#=============================================================================================
+# Caso a versão do debian já tenha sido baixada, não baixar novamente
+if [ -d "$folder" ]; then
+	first=1
+	echo "${label_skip_download}"
+fi
+
+sleep 2
+# Baixa
+if [ "$first" != 1 ];then
+	case `dpkg --print-architecture` in
+	aarch64)
+		archurl="arm64" ;;
+	arm)
+		archurl="armhf" ;;
+	*)
+		echo "unknown architecture"; exit 1 ;;
+	esac
+	show_progress_dialog wget "${label_alpine_download}" 1 -O $folder.tar.xz "${extralink}/distros/files/dists/${archurl}/alpine/${codinome}/installer.tar.xz"
+	sleep 2
+	show_progress_dialog extract "${label_alpine_download_extract}" "$HOME/$folder.tar.xz"
+	sleep 2
+fi
