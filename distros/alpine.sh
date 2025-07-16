6ef9f22ed0ad779ce3c6b8e09f6415a156df0d68
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-07-16 02:01:31 -0300

    v0.0.67.5.36
    * testanto configurações para instalar o alpine

--- distros/alpine.sh
+++ distros/alpine.sh
@@ -3,6 +3,7 @@ source "$PREFIX/bin/andistro_files/global_var_fun.sh"
 bin="start-alpine.sh"
 codinome="edge"
 folder="alpine-edge"
+binds="alpine-binds"
 
 #=============================================================================================
 # Caso a versão do debian já tenha sido baixada, não baixar novamente
@@ -27,3 +28,60 @@ if [ "$first" != 1 ];then
 	show_progress_dialog extract "${label_alpine_download_extract}" "$HOME/$folder.tar.xz"
 	sleep 2
 fi
+
+cat > $bin <<- EOM
+	#!/data/data/com.termux/files/usr/bin/bash
+	cd \$(dirname \$0)
+	## unset LD_PRELOAD in case termux-exec is installed
+	unset LD_PRELOAD
+	command="proot"
+	command+=" --link2symlink"
+	command+=" -0"
+	command+=" -r $folder"
+	if [ -n "\$(ls -A $binds)" ]; then
+    		for f in $binds/* ;do
+      		. \$f
+    	done
+	fi
+	command+=" -b /dev"
+	command+=" -b /proc"
+	command+=" -b $folder/root:/dev/shm"
+	## uncomment the following line to have access to the home directory of termux
+	#command+=" -b /data/data/com.termux/files/home:/root"
+	## uncomment the following line to mount /sdcard directly to /
+	#command+=" -b /sdcard"
+	command+=" -w /root"
+	command+=" /usr/bin/env -i"
+	command+=" HOME=/root"
+	command+=" PATH=/bin:/usr/bin:/sbin:/usr/sbin"
+	command+=" TERM=\$TERM"
+	command+=" LANG=en_US.UTF-8"
+	command+=" LC_ALL=C"
+	command+=" LANGUAGE=en_US"
+	command+=" /bin/sh --login"
+	com="\$@"
+	if [ -z "\$1" ];then
+    		exec \$command
+	else
+    		\$command -c "\$com"
+	fi
+	EOM
+
+chmod +x $bin
+
+echo "" > $folder/etc/fstab
+rm -rf $folder/etc/resolv.conf
+echo nameserver 8.8.8.8 > $folder/etc/resolv.conf
+
+./$bin apk update
+        ./$bin apk add --no-cache bash
+        sed -i 's/ash/bash/g' $folder/etc/passwd
+        sed -i 's/bin\/sh/bin\/bash/g' $bin
+	echo "Installation Finished"
+	rm -rf $folder/root/.bash_profile
+  	echo "#!/bin/bash
+              wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Alpine/alpine-xfce.sh -O /root/alpine-xfce.sh
+              bash /root/alpine-xfce.sh
+              rm -rf /root/alpine-xfce.sh
+              clear" > $folder/root/.bash_profile  
+   	bash $bin
