9d59309a509ad592b880349b948570304d964665
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-07-18 01:22:44 -0300

    .

--- config/package-manager-setups/apk/environment/xfce4/config.sh
+++ config/package-manager-setups/apk/environment/xfce4/config.sh
@@ -3,22 +3,10 @@
 source "/usr/local/bin/global_var_fun.sh"
 mkdir -p "$HOME/.vnc"
 
-show_progress_dialog steps-one-label "${label_install_environment_gui}" 7 \
+show_progress_dialog steps-one-label "${label_install_environment_gui}" 3 \
      'sudo apk add --no-cache xfce4' \
      'sudo apk add --no-cache xfce4-terminal' \
-     'sudo apk add --no-cache xfce4-appfinder' \
-     'bash -c "cat > $HOME/.vnc/xstartup <<EOF
-#!/bin/bash
-export PULSE_SERVER=127.0.0.1
-export LANG
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-echo $$ > /tmp/xsession.pid
-startxfce4
-EOF
-"' \
-     'chmod +x /usr/local/bin/vnc' \
-     "echo 'export DISPLAY=":1"' >> /etc/profile"
+     'sudo apk add --no-cache xfce4-appfinder'
 sleep 2
 
 vncpasswd
