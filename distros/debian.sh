b77e5f3a841e009f5c7c1af0f3d0b39fcbe9d7c0
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-09-02 00:38:06 -0300

    melhorias no código para que seja visualizado o ip de acesso ao servidor

--- distros/debian.sh
+++ distros/debian.sh
@@ -85,11 +85,11 @@ fi
 echo "${label_start_script}"
 cat > $bin <<- EOM
 #!/bin/bash
-source "$PREFIX/bin/andistro_files/global_var_fun.sh"
-sed -i "s|WLAN_IP=\"localhost\"|WLAN_IP=\"\$wlan_ip_localhost\"|g" "$folder/usr/local/bin/vnc"
+source "\$PREFIX/bin/andistro_files/global_var_fun.sh"
+sed -i "s|WLAN_IP=\\\"localhost\\\"|WLAN_IP=\\\"\$wlan_ip_localhost\\\"|g" "$folder/usr/local/bin/vnc"
 
 #cd \$(dirname \$0)
-cd $HOME
+cd \$HOME
 ## unset LD_PRELOAD in case termux-exec is installed
 
 unset LD_PRELOAD
@@ -97,10 +97,10 @@ command="proot"
 command+=" --kill-on-exit"
 command+=" --link2symlink"
 command+=" -0"
-command+=" -r $folder"
+command+=" -r \$HOME/$folder"
 command+=" -b /dev"
 command+=" -b /proc"
-command+=" -b $folder/root:/dev/shm"
+command+=" -b \$HOME/$folder/root:/dev/shm"
 ## uncomment the following line to have access to the home directory of termux
 #command+=" -b /data/data/com.termux/files/home:/root"
 ## uncomment the following line to mount /sdcard directly to / 
@@ -110,10 +110,11 @@ command+=" /usr/bin/env -i"
 command+=" HOME=/root"
 command+=" PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
 command+=" TERM=\$TERM"
-command+=" LANG=C.UTF-8"
+#command+=" LANG=C.UTF-8"
+command+=" LANG=$language_transformed.UTF-8"
 command+=" /bin/bash --login"
 com="\$@"
-if [ -z "\$1" ];then
+if [ -z "\$1" ]; then
     exec \$command
 else
     \$command -c "\$com"
@@ -169,11 +170,7 @@ chmod +x "$folder/root/system-config.sh"
 sed -i "s/system_icu_locale_code=.*$/system_icu_locale_code=\"${language_selected}\"/" "$folder/usr/local/bin/global_var_fun.sh"
 sleep 2
 
-#Copiando arquivos para dentro do linux
 
-#echo "fixing shebang of $bin"
-#termux-fix-shebang $bin
-#echo "making $bin executable"
 chmod +x $bin
 
 export USER=$(whoami)
