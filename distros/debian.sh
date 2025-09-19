55edbb9db7e1c7d4b43755e4b9f2c9a36b7cb616
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-09-19 04:40:13 -0300

    ajustes no código

--- distros/debian.sh
+++ distros/debian.sh
@@ -291,11 +291,14 @@ rm -rf ~/.hushlogin
 rm -rf ~/system-config.sh
 rm -rf ~/config-environment.sh
 rm -rf ~/start-environment.sh
-#rm -rf $HOME/start-distro.sh
+rm -rf /termux-home/start-distro.sh
 EOM
 
 
 # Cria uma gui de inicialização
 sed -i '\|command+=" /bin/bash --login"|a command+=" -b /usr/local/bin/startvncserver"' $bin
 bash $bin
-#rm -rf $HOME/start-distro.sh
+
+if [ -e "$HOME/start-distro.sh" ];then
+	rm -rf $HOME/start-distro.sh
+fi
