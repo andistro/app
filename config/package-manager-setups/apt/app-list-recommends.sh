5a267aae28b08eda9d95365e68a14e5c88aa6547
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-09-25 19:44:08 -0300

    Adicionado o visual studio code a lista

--- config/package-manager-setups/apt/app-list-recommends.sh
+++ config/package-manager-setups/apt/app-list-recommends.sh
@@ -2,12 +2,13 @@
 source "/usr/local/bin/global"
 PROGS=(
  "brave-browser" "Brave Browser" off
+ "code" "Visual Studio Code" off
  "figma-linux" "Figma Linux" off
  "inkscape" "Inkscape" off
  "libreoffice" "LibreOffice" off
 )
 CHOICES=$(dialog --no-shadow --separate-output \
- --checklist "Selecione os programas para instalar:" 10 50 7 \
+ --checklist "Selecione os programas para instalar:" 50 50 7 \
  "${PROGS[@]}" \
  2>&1 >/dev/tty)
 
@@ -21,26 +22,34 @@ for chr in $CHOICES; do
  "figma-linux")
    log_file="/sdcard/termux/andistro/logs/app-recommends-figma-linux_${timestamp}.txt"
    ETAPAS+=(
-     "Instalando dependência libgconf-2-4" "sudo apt install libgconf-2-4 --no-install-recommends -y >> $log_file 2>&1"
-     "Baixando Figma-Linux (.deb)" "wget -O /tmp/figma-linux.deb https://github.com/Figma-Linux/figma-linux/releases/download/v0.11.5/figma-linux_0.11.5_linux_arm64.deb >> $log_file 2>&1"
-     "Instalando Figma-Linux" "sudo dpkg -i /tmp/figma-linux.deb ; sudo apt install -f -y >> $log_file 2>&1"
-     "Ajustando atalho Figma" "sudo sed -i 's|Exec=/usr/share/figma-linux/figma-linux|Exec=/usr/share/figma-linux/figma-linux --no-sandbox|' /usr/share/applications/figma-linux.desktop >> $log_file 2>&1"
+     "${label_installing}\n\n → Figma Linux" "sudo apt install libgconf-2-4 --no-install-recommends -y >> $log_file 2>&1"
+     "${label_installing}\n\n → Figma Linux" "wget -O /tmp/figma-linux.deb https://github.com/Figma-Linux/figma-linux/releases/download/v0.11.5/figma-linux_0.11.5_linux_arm64.deb >> $log_file 2>&1"
+     "${label_installing}\n\n → Figma Linux" "sudo dpkg -i /tmp/figma-linux.deb ; sudo apt install -f -y >> $log_file 2>&1"
+     "${label_setting_up}\n\n → Figma Linux" "sudo sed -i 's|Exec=/usr/share/figma-linux/figma-linux|Exec=/usr/share/figma-linux/figma-linux --no-sandbox|' /usr/share/applications/figma-linux.desktop >> $log_file 2>&1"
    )
    NUM_ETAPAS=$((NUM_ETAPAS + 4))
   ;;
  "brave-browser")
    log_file="/sdcard/termux/andistro/logs/app-recommends-brave-browser_${timestamp}.txt"
    ETAPAS+=(
-     "Instalando Brave Browser" "sudo apt install brave-browser --no-install-recommends -y >> $log_file 2>&1"
-     "Ajustando atalho Brave" "sudo sed -i 's|Exec=/usr/bin/brave-browser-stable|Exec=/usr/bin/brave-browser-stable --no-sandbox|' /usr/share/applications/brave-browser.desktop >> $log_file 2>&1"
-     "Ajustando atalho Brave 2" "sudo sed -i 's|Exec=/usr/bin/brave-browser-stable|Exec=/usr/bin/brave-browser-stable --no-sandbox|' /usr/share/applications/com.brave.Browser.desktop >> $log_file 2>&1"
+     "${label_installing}\n\n → Brave Browser" "sudo apt install brave-browser --no-install-recommends -y >> $log_file 2>&1"
+     "${label_setting_up}\n\n → Brave Browser" "sudo sed -i 's|Exec=/usr/bin/brave-browser-stable|Exec=/usr/bin/brave-browser-stable --no-sandbox|' /usr/share/applications/brave-browser.desktop >> $log_file 2>&1"
+     "${label_setting_up}\n\n → Brave Browser" "sudo sed -i 's|Exec=/usr/bin/brave-browser-stable|Exec=/usr/bin/brave-browser-stable --no-sandbox|' /usr/share/applications/com.brave.Browser.desktop >> $log_file 2>&1"
+   )
+   NUM_ETAPAS=$((NUM_ETAPAS + 3))
+  ;;
+  "code")
+   log_file="/sdcard/termux/andistro/logs/app-recommends-code_${timestamp}.txt"
+   ETAPAS+=(
+     "${label_installing}" "sudo apt install code --no-install-recommends -y >> $log_file 2>&1"
+     "${label_setting_up}" "sudo sed -i 's|Exec=/usr/share/code/code|Exec=/usr/share/code/code --no-sandbox|' /usr/share/applications/code*.desktop >> $log_file 2>&1"
    )
    NUM_ETAPAS=$((NUM_ETAPAS + 3))
   ;;
  *)
    log_file="/sdcard/termux/andistro/logs/app-recommends-${chr}_${timestamp}.txt"
    ETAPAS+=(
-     "Instalando $chr" "sudo apt install $chr --no-install-recommends -y >> $log_file 2>&1"
+     "${label_installing} \n\n → $chr" "sudo apt install $chr --no-install-recommends -y >> $log_file 2>&1"
    )
    NUM_ETAPAS=$((NUM_ETAPAS + 1))
   ;;
