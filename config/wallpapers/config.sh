3c2743f912ce80ec0b3be5f8b8fe523caa148957
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-09-25 18:05:20 -0300

    Baixar wallpapers

--- config/wallpapers/config.sh
+++ config/wallpapers/config.sh
@@ -6,4 +6,4 @@ show_progress_dialog steps-multi-label 4 \
     "${label_wallpaper_download}\n\n → Unsplash: " 'mkdir -p /usr/share/backgrounds/unsplash'\
     "${label_wallpaper_download}\n\n → Unsplash: " 'mkdir -p /usr/share/backgrounds/unsplash/vertical'\
     "${label_wallpaper_download}\n\n → Unsplash: " 'mkdir -p /usr/share/backgrounds/unsplash/horizontal'\
-    "${label_wallpaper_download}\n\n → Unsplash: " 'wget -O "/usr/share/background/unsplash/horizontal/martz90-rMRT4hF-Fsg.jpg" "https://unsplash.com/photos/rMRT4hF-Fsg/download"'
+    "${label_wallpaper_download}\n\n → Unsplash: martz90-rMRT4hF-Fsg" 'wget -O "/usr/share/background/unsplash/horizontal/martz90-rMRT4hF-Fsg.jpg" "https://unsplash.com/photos/rMRT4hF-Fsg/download"'
