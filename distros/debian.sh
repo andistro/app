47f27963542756f0142e2258b64e743a7a21a4aa
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-09-26 03:16:41 -0300

    Ajuste do debian

--- distros/debian.sh
+++ distros/debian.sh
@@ -132,7 +132,7 @@ cd \$HOME
 #Start termux-x11
 #termux-x11 :1 &
 
-pulseaudio --start --exit-idle-time=-1
+pulseaudio --start --exit-idle-time=20
 PA_PID=\$(pgrep pulseaudio)
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 pacmd load-module module-aaudio-sink
