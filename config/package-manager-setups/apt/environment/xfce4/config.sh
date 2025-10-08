69cc62ca6b429e754a9590dd0d0fde3342ead231
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-10-07 22:33:35 -0300

    melhorias no código

--- config/package-manager-setups/apt/environment/xfce4/config.sh
+++ config/package-manager-setups/apt/environment/xfce4/config.sh
@@ -53,25 +53,24 @@ fi
 source /etc/profile
 
 show_progress_dialog steps-one-label "${label_config_environment_gui}" 22 \
-  "vncserver -name remote-desktop -geometry 1920x1080 :1" \
-  "sleep 10" \
-  "sleep 4" \
-  'xfconf-query -c xsettings -p /Net/ThemeName -s AnDistro-Majorelle-Blue-${distro_theme}' \
-  "sleep 4" \
-  'xfconf-query -c xsettings -p /Net/IconThemeName -s AnDistro-Majorelle-Blue-${distro_theme}' \
-  "sleep 4" \
-  'xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVNC-0/workspace0/last-image --create --type string --set "/usr/share/backgrounds/${wallpaper}"' \
-  'wget --tries=20 "${extralink}/config/package-manager-setups/apt/environment/xfce4/xfce4-panel.tar.bz2"  -O ~/xfce4-panel.tar.bz2 > /dev/null 2>&1' \
-  "chmod +x ~/xfce4-panel.tar.bz2" \
-  "dbus-launch --exit-with-session xfce4-panel-profiles load xfce4-panel.tar.bz2" \
-  "xfce4-panel-profiles load xfce4-panel.tar.bz2" \
-  "sleep 4" \
-  "firefox > /dev/null 2>&1 & PID=$!; sleep 5; kill $PID" \
-  "sed -i '/security.sandbox.content.level/d' ~/.mozilla/firefox/*.default-release/prefs.js" \
-  'echo "user_pref(\"security.sandbox.content.level\", 0);" >> ~/.mozilla/firefox/*.default-release/prefs.js' \
-  "sudo apt-get clean" \
-  "vncserver -kill :1" \
-  "rm -rf /tmp/.X*-lock" \
-  "rm -rf /tmp/.X11-unix/X*" \
-  "rm -rf ~/start-environment.sh" \
-  "rm -rf ~/xfce4-panel.tar.bz2"
+     "sleep 4" \
+     'dbus-launch --exit-with-session xfconf-query -c xsettings -p /Net/ThemeName -s AnDistro-Majorelle-Blue-${distro_theme}' \
+     "sleep 4" \
+     'dbus-launch --exit-with-session xfconf-query -c xsettings -p /Net/IconThemeName -s AnDistro-Majorelle-Blue-${distro_theme}' \
+     "sleep 4" \
+     'dbus-launch --exit-with-session xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVNC-0/workspace0/last-image --create --type string --set "/usr/share/backgrounds/${wallpaper}"' \
+     'wget --tries=20 "${extralink}/config/package-manager-setups/apt/environment/xfce4/xfce4-panel.tar.bz2"  -O ~/xfce4-panel.tar.bz2 > /dev/null 2>&1' \
+     "chmod +x ~/xfce4-panel.tar.bz2" \
+     "dbus-launch --exit-with-session xfce4-panel-profiles load xfce4-panel.tar.bz2" \
+     "sleep 4" \
+     "vncserver -name remote-desktop -geometry 1920x1080 :1" \
+     "sleep 10" \
+     "dbus-launch --exit-with-session firefox > /dev/null 2>&1 & PID=$!; sleep 5; kill $PID" \
+     "sed -i '/security.sandbox.content.level/d' ~/.mozilla/firefox/*.default-release/prefs.js" \
+     'echo "user_pref(\"security.sandbox.content.level\", 0);" >> ~/.mozilla/firefox/*.default-release/prefs.js' \
+     "sudo apt-get clean" \
+     "vncserver -kill :1" \
+     "rm -rf /tmp/.X*-lock" \
+     "rm -rf /tmp/.X11-unix/X*" \
+     "rm -rf ~/start-environment.sh" \
+     "rm -rf ~/xfce4-panel.tar.bz2"
