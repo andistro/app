7966780e65ee26b22603722884172f4b73b7329b
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-10-09 00:54:29 -0300

    atualização do código

--- config/locale/install.sh
+++ config/locale/install.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-if [ -f "$PREFIX/var/lib/andistro/lib/share/global" ]; then
-    chmod +x "$PREFIX/var/lib/andistro/lib/share/global"
-    source "$PREFIX/var/lib/andistro/lib/share/global"
-    mkdir -p "$PREFIX/var/lib/andistro/lib/share/locales/"
-    path="$PREFIX/var/lib/andistro/lib/share/locales/"
-elif [ -f "/usr/local/bin/global" ]; then
-    chmod +x "/usr/local/bin/global"
-    source "/usr/local/bin/global"
-    mkdir -p "/usr/local/bin/locales"
-    path="/usr/local/bin/locales"
-else
-    echo "Erro: arquivo global de configuração não encontrado."
-fi
+source "$PREFIX/var/lib/andistro/lib/share/global"    
+mkdir -p "$PREFIX/var/lib/andistro/lib/share/locales/"
+path="$PREFIX/var/lib/andistro/lib/share/locales"
 
 show_progress_dialog "wget" "${label_language_download}" 1 \
-    -P "$path" "${extralink}/config/package-manager-setups/apt/locale/locale_en-US.sh" \
-    -P "$path" "${extralink}/config/package-manager-setups/apt/locale/locale_pt-BR.sh"
+    -P "$path" "${extralink}/config/locale/l10n_en-US.sh" \
+    -P "$path" "${extralink}/config/locale/l10n_pt-BR.sh"
 
-chmod +x "$path/locale_*.sh"
+chmod +x "$path/l10n_*.sh"
