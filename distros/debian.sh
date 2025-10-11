2f1287831a3aa5fd489ca61d6a209c664817fd53
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-10-11 00:27:52 -0300

    ajustes

--- distros/debian.sh
+++ distros/debian.sh
@@ -133,8 +133,10 @@ cd \$HOME
 #termux-x11 :1 &
 
 pulseaudio --start --exit-idle-time=-1
+PA_PID=\$(pgrep pulseaudio)
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 pacmd load-module module-aaudio-sink
+
 ## unset LD_PRELOAD in case termux-exec is installed
 unset LD_PRELOAD
 command="proot"
