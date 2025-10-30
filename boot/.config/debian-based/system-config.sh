c3b6f78800e0e619a726abca71a5743b74b5cbec
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-10-30 01:11:54 -0300

    🐞 adiciona parâmetro language_selected ao script system-config.sh e ajusta a geração de pacotes

--- boot/.config/debian-based/system-config.sh
+++ boot/.config/debian-based/system-config.sh
@@ -1,43 +1,46 @@
 #!/bin/bash
 export distro_theme="$1"
 export distro_name="$2"
+
+apt_system_icu_locale_code=$(echo "$LANG" | sed 's/\..*//' | sed 's/_/-/' | tr '[:upper:]' '[:lower:]')
 # Fonte modular configuração global
 source "/usr/local/bin/andistro/global"
 
 # Baixa os pacotes base, um por um
-show_progress_dialog steps-multi-label 67 \
+show_progress_dialog steps-multi-label 48 \
   "${label_progress}" 'echo "system-config"' \
   "${label_progress}" 'sudo apt clean' \
   "${label_find_update}" 'sudo apt update' \
   "${label_upgrade}" 'sudo apt full-upgrade -y' \
   "${label_install_script_download}\n\n → apt-utils" 'sudo apt install apt-utils --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → debconf-utils" 'sudo apt install debconf-utils --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → dbus-x11" 'sudo apt install dbus-x11 --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → python3" 'sudo apt install python3 --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → python3-psutil" 'sudo apt install python3-psutil --no-install-recommends -y' \
   "${label_install_script_download}\n\n → at-spi2-core" 'sudo apt install at-spi2-core --no-install-recommends -y' \
   "${label_install_script_download}\n\n → bleachbit" 'sudo apt install bleachbit --no-install-recommends -y' \
-  "${label_install_script_download}\n\n → code" 'sudo apt install code --no-install-recommends -y' \
-  "${label_install_script_download}\n\n → dbus-x11" 'sudo apt install dbus-x11 --no-install-recommends -y' \
   "${label_install_script_download}\n\n → exo-utils" 'sudo apt install exo-utils --no-install-recommends -y' \
   "${label_install_script_download}\n\n → firefox" 'sudo apt install firefox --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → firefox ${apt_system_icu_locale_code}" "sudo apt install firefox-l10n-${apt_system_icu_locale_code} --no-install-recommends -y" \
   "${label_install_script_download}\n\n → font-manager" 'sudo apt install font-manager --no-install-recommends -y' \
   "${label_install_script_download}\n\n → git" 'sudo apt install git --no-install-recommends -y' \
-  "${label_install_script_download}\n\n → gvfs-backends" 'sudo apt install gvfs-backends --no-install-recommends -y' \
   "${label_install_script_download}\n\n → inetutils-tools" 'sudo apt install inetutils-tools --no-install-recommends -y' \
-  "${label_install_script_download}\n\n → keyboard-configuration" "sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration --no-install-recommends -y" \
   "${label_install_script_download}\n\n → lsb-release" 'sudo apt install lsb-release --no-install-recommends -y' \
   "${label_install_script_download}\n\n → make" 'sudo apt install make --no-install-recommends -y' \
   "${label_install_script_download}\n\n → net-tools" 'sudo apt install net-tools --no-install-recommends -y' \
   "${label_install_script_download}\n\n → pavucontrol" 'sudo apt install pavucontrol --no-install-recommends -y' \
-  "${label_install_script_download}\n\n → python3" 'sudo apt install python3 --no-install-recommends -y' \
   "${label_install_script_download}\n\n → synaptic" 'sudo apt install synaptic --no-install-recommends -y' \
   "${label_install_script_download}\n\n → tigervnc-common" 'sudo apt install tigervnc-common --no-install-recommends -y' \
   "${label_install_script_download}\n\n → tigervnc-standalone-server" 'sudo apt install tigervnc-standalone-server --no-install-recommends -y' \
   "${label_install_script_download}\n\n → tigervnc-tools" 'sudo apt install tigervnc-tools --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → trash-cli" 'sudo apt install trash-cli --no-install-recommends -y' \
   "${label_install_script_download}\n\n → tumbler" 'sudo apt install tumbler --no-install-recommends -y' \
+  "${label_install_script_download}\n\n → ffmpegthumbnailer" 'sudo apt install ffmpegthumbnailer --no-install-recommends -y' \
   "${label_install_script_download}\n\n → unzip" 'sudo apt install unzip --no-install-recommends -y' \
   "${label_install_script_download}\n\n → xdg-user-dirs" 'sudo apt install xdg-user-dirs --no-install-recommends -y' \
   "${label_install_script_download}\n\n → xz-utils" 'sudo apt install xz-utils --no-install-recommends -y' \
   "${label_install_script_download}\n\n → zip" 'sudo apt install zip --no-install-recommends -y' \
   "${label_install_script_download}" 'sleep 5' \
-  "${label_system_setup}\n\n → code: code" "sudo sed -i 's|Exec=/usr/share/code/code|Exec=/usr/share/code/code --no-sandbox|' /usr/share/applications/code*.desktop" \
   "${label_system_setup}\n\n → synaptic" "sudo sed -i 's/^Exec=synaptic-pkexec/Exec=synaptic/' /usr/share/applications/synaptic.desktop" \
   "${label_system_setup}\n\n → ${label_themes}: andistro-themes" 'git clone https://github.com/andistro/themes.git' \
   "${label_system_setup}\n\n → ${label_themes}: andistro-themes" 'mv themes/AnDistro*/ /usr/share/themes/' \
@@ -55,7 +58,7 @@ show_progress_dialog steps-multi-label 67 \
   "${label_system_setup}" 'sudo apt-get clean'
 
 sleep 2
-
+#  "${label_install_script_download}\n\n → gvfs-backends" 'sudo apt install gvfs-backends --no-install-recommends -y' \
 #show_progress_dialog check-packages "Verificando" tzdata xz-utils keyboard-configuration wget curl gpg git python3 tar unzip zip apt-utils lsb-release exo-utils tigervnc-standalone-server tigervnc-common tigervnc-tools dbus-x11 nano inetutils-tools net-tools font-manager synaptic gvfs-backends bleachbit pulseaudio pavucontrol make tumbler firefox code
 
 # Tempo de 10s antes de inicializar as configurações do teclado
@@ -65,7 +68,9 @@ sleep 2
    echo $((i * 2))
  done
 } | dialog --no-shadow --gauge "$label_keyboard_setup" 10 60 0
-sudo dpkg-reconfigure keyboard-configuration
+sudo apt install keyboard-configuration --no-install-recommends -y
+
+
 
 # Remove o arquivo do sistema
 rm -rf system-config.sh
