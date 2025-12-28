ba666ebaf4131c75465ebe93d35c25f639c84e64
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2025-12-28 19:36:53 -0300

    Ajustes nos termos de uso

--- termos.sh
+++ termos.sh
@@ -16,33 +16,33 @@ if [ "$system_lang_code" = "en-US" ]; then
 cat > "$TERMO" << EOF
 TERMS OF USE
 
-1. By using the AnDistro installer, the user understands that:
+By using the AnDistro installer, the user understands that:
 
-2. The project is not responsible for the use of the application on your device or for any potential damages.
+- The project is not responsible for the use of the application on your device or for any potential damages.
 
-3. The installer does not collect any user data during its operation and is not responsible for third parties.
+- The installer does not collect any user data during its operation and is not responsible for third parties.
 
-4. All source code is open and available in the official GitHub repository for inspection and verification.
+- All source code is open and available in the official GitHub repository for inspection and verification.
 
-5. Not all Android devices are guaranteed to work properly, as technical limitations may occur due to different system versions and restrictions imposed by Android itself.
+- Not all Android devices are guaranteed to work properly, as technical limitations may occur due to different system versions and restrictions imposed by Android itself.
 
-6. By proceeding, the user acknowledges these conditions.
+- By proceeding, the user acknowledges these conditions.
 EOF
 elif [ "$system_lang_code" = "pt-BR" ]; then
 cat > "$TERMO" << EOF
 TERMOS DE USO
 
-1. Ao utilizar o instalador AnDistro, o usuário entende que:
+Ao utilizar o instalador AnDistro, o usuário entende que:
 
-2. O projeto não se responsabiliza pelo uso do aplicativo em seu dispositivo ou por eventuais danos.
+- O projeto não se responsabiliza pelo uso do aplicativo em seu dispositivo ou por eventuais danos.
 
-3. O instalador não coleta nenhum dado do usuário durante o uso do instalador e não se responsabiliza por terceiros.
+- O instalador não coleta nenhum dado do usuário durante o uso do instalador e não se responsabiliza por terceiros.
 
-4. Todo o código-fonte é aberto e está disponível no repositório oficial do GitHub para consulta e verificação.
+- Todo o código-fonte é aberto e está disponível no repositório oficial do GitHub para consulta e verificação.
 
-5. Nem todos os dispositivos Android terão funcionamento garantido, podendo ocorrer limitações técnicas devido às diferentes versões do sistema e restrições impostas pelo próprio Android.
+- Nem todos os dispositivos Android terão funcionamento garantido, podendo ocorrer limitações técnicas devido às diferentes versões do sistema e restrições impostas pelo próprio Android.
 
-6. Ao prosseguir, o usuário reconhece estas condições.
+- Ao prosseguir, o usuário reconhece estas condições.
 EOF
 fi
 
@@ -62,7 +62,7 @@ fi
 
 
 dialog --no-shadow --title "$label_term" --yes-label "$label_accept" --no-label "$label_reject" \
-       --yesno "$(cat $TERMO)" 20 70
+       --yesno "$(cat $TERMO)" 20 100
 
 resposta=$?
 
@@ -74,7 +74,7 @@ if [ $resposta -eq 0 ]; then
     # aqui é onde você coloca o que já seria feito
 elif [ $resposta -eq 1 ]; then
     # Usuário recusou
-    dialog --no-shadow --msgbox "$label_u_rejected" 10 60
+    dialog --no-shadow --msgbox "$label_u_rejected" 20 100
     andistro -d total
 fi
 
