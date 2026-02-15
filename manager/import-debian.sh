1e6e6ec385b4c66c26286dc3c49e6cb2a4a663c6
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2026-02-15 20:56:17 -0300

    Corrigindo falhas da importação do debian proot para o andistro

--- manager/import-debian.sh
+++ manager/import-debian.sh
@@ -24,30 +24,27 @@ label_distro_download_start=$(printf "$label_distro_download_start" "Debian")
 label_distro_download_finish=$(printf "$label_distro_download_finish" "Debian")
 
 if [ "$first" != 1 ];then
-mkdir -p "$folder"
-echo "$folder"
-sleep 5
 show_progress_dialog steps-one-label "Copiando o Debian do Proot-Distro e baixando pacotes necessários para o Andistro" 20 \
     'sleep 1' \
     'sleep 1' \
-    'sleep 1' \
+    "mkdir -p \"$folder\"" \
     'cp -a "/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian"/* "/data/data/com.termux/files/usr/var/lib/andistro/manager/debian/stable/"' \
     'sleep 5' \
-    'cp "$config_file/start-distro" $bin' \
-    'chmod +x $bin' \
-    'bash $bin apt update' \
-    'bash $bin apt install dialog sudo wget nano locales gpg curl ca-certificates -y' \
-    'sed -i "s|command+=\" LANG=\$system_icu_lang_code_env.UTF-8\"|command+=\" LANG=$system_icu_lang_code_env.UTF-8\"|g" $bin' \
-    'rm -rf $folder/root/.bash_profile' \
-    'cp "$config_file/.bash_profile" $folder/root/.bash_profile' \
-    'sed -i "s|distro_name=\"\"|distro_name=\"$distro_name\"|g" $folder/root/.bash_profile' \
-    'sed -i "s|LANG=\"\"|LANG=\"$system_icu_lang_code_env.UTF-8\"|g" $folder/root/.bash_profile' \
-    'cp $config_file/system-config.sh $folder/root/system-config.sh' \
-    'echo "127.0.0.1 localhost localhost" | tee $folder/etc/hosts' \
-    'echo "nameserver 8.8.8.8" | tee $folder/etc/resolv.conf' \
-    'echo "$system_timezone" | tee $folder/etc/timezone' \
-    'echo "APT::Acquire::Retries \"3\";" > $folder/etc/apt/apt.conf.d/80-retries' \
-    'touch $folder/root/.hushlogin'
+    "cp \"$config_file/start-distro\" $bin" \
+    "chmod +x $bin" \
+    "bash $bin apt update" \
+    "bash $bin apt install dialog sudo wget nano locales gpg curl ca-certificates -y" \
+    "sed -i \"s|command+=\" LANG=\$system_icu_lang_code_env.UTF-8\"|command+=\" LANG=$system_icu_lang_code_env.UTF-8\"|g\" $bin" \
+    "rm -rf $folder/root/.bash_profile" \
+    "cp \"$config_file/.bash_profile\" $folder/root/.bash_profile" \
+    "sed -i \"s|distro_name=\"\"|distro_name=\"$distro_name\"|g\" $folder/root/.bash_profile" \
+    "sed -i \"s|LANG=\"\"|LANG=\"$system_icu_lang_code_env.UTF-8\"|g\" $folder/root/.bash_profile" \
+    "cp $config_file/system-config.sh $folder/root/system-config.sh" \
+    "echo \"127.0.0.1 localhost localhost\" | tee $folder/etc/hosts" \
+    "echo \"nameserver 8.8.8.8\" | tee $folder/etc/resolv.conf" \
+    "echo \"$system_timezone\" | tee $folder/etc/timezone" \
+    "echo \"APT::Acquire::Retries \"3\";\" > $folder/etc/apt/apt.conf.d/80-retries" \
+    "touch $folder/root/.hushlogin"
 fi
 
 
