404076c7ff2c8d28e000b988f2fd12f15aea0508
Tiago José <47113005+soutiagojose@users.noreply.github.com>
2026-02-17 18:05:10 -0300

    Corrigindo falhas da importação do debian proot para o andistro

--- manager/import-debian.sh
+++ manager/import-debian.sh
@@ -25,6 +25,29 @@ label_distro_download_finish=$(printf "$label_distro_download_finish" "Debian")
 
 if [ "$first" != 1 ];then
 
+show_progress_dialog steps-one-label "Copiando o Debian do Proot-Distro e baixando pacotes necessários para o Andistro" 20 \
+    'sleep 1' \
+    'sleep 1' \
+    "mkdir -p \"$folder\"" \
+    'cp -a "/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian"/* "/data/data/com.termux/files/usr/var/lib/andistro/manager/debian/stable/"' \
+    'sleep 5' \
+    "cp \"$config_file/start-distro\" $bin" \
+    "chmod +x $bin" \
+    "bash $bin apt update" \
+    "bash $bin apt install dialog sudo wget nano locales gpg curl ca-certificates -y" \
+    "sed -i \"s|command+=\" LANG=\$system_icu_lang_code_env.UTF-8\"|command+=\" LANG=$system_icu_lang_code_env.UTF-8\"|g\" $bin" \
+    "rm -rf $folder/root/.bash_profile" \
+    "cp \"$config_file/.bash_profile\" $folder/root/.bash_profile" \
+    "sed -i \"s|distro_name=\"\"|distro_name=\"$distro_name\"|g\" $folder/root/.bash_profile" \
+    "sed -i \"s|LANG=\"\"|LANG=\"$system_icu_lang_code_env.UTF-8\"|g\" $folder/root/.bash_profile" \
+    "cp $config_file/system-config.sh $folder/root/system-config.sh" \
+    "echo \"127.0.0.1 localhost localhost\" | tee $folder/etc/hosts" \
+    "echo \"nameserver 8.8.8.8\" | tee $folder/etc/resolv.conf" \
+    "echo \"$system_timezone\" | tee $folder/etc/timezone" \
+    "echo \"APT::Acquire::Retries \"3\";\" > $folder/etc/apt/apt.conf.d/80-retries" \
+    "touch $folder/root/.hushlogin"
+fi
+
 # Verifica se existe LANG em /etc/environment e substitui por pt_BR.UTF-8
 if grep -q "^LANG=" $folder/etc/environment 2>/dev/null; then
     sed -i "s/^LANG=.*$/LANG=$system_icu_lang_code_env.UTF-8/" $folder/etc/environment
@@ -70,29 +93,6 @@ if [ -d /etc/profile.d ]; then
   unset i
 fi
 EOF
-
-show_progress_dialog steps-one-label "Copiando o Debian do Proot-Distro e baixando pacotes necessários para o Andistro" 20 \
-    'sleep 1' \
-    'sleep 1' \
-    "mkdir -p \"$folder\"" \
-    'cp -a "/data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian"/* "/data/data/com.termux/files/usr/var/lib/andistro/manager/debian/stable/"' \
-    'sleep 5' \
-    "cp \"$config_file/start-distro\" $bin" \
-    "chmod +x $bin" \
-    "bash $bin apt update" \
-    "bash $bin apt install dialog sudo wget nano locales gpg curl ca-certificates -y" \
-    "sed -i \"s|command+=\" LANG=\$system_icu_lang_code_env.UTF-8\"|command+=\" LANG=$system_icu_lang_code_env.UTF-8\"|g\" $bin" \
-    "rm -rf $folder/root/.bash_profile" \
-    "cp \"$config_file/.bash_profile\" $folder/root/.bash_profile" \
-    "sed -i \"s|distro_name=\"\"|distro_name=\"$distro_name\"|g\" $folder/root/.bash_profile" \
-    "sed -i \"s|LANG=\"\"|LANG=\"$system_icu_lang_code_env.UTF-8\"|g\" $folder/root/.bash_profile" \
-    "cp $config_file/system-config.sh $folder/root/system-config.sh" \
-    "echo \"127.0.0.1 localhost localhost\" | tee $folder/etc/hosts" \
-    "echo \"nameserver 8.8.8.8\" | tee $folder/etc/resolv.conf" \
-    "echo \"$system_timezone\" | tee $folder/etc/timezone" \
-    "echo \"APT::Acquire::Retries \"3\";\" > $folder/etc/apt/apt.conf.d/80-retries" \
-    "touch $folder/root/.hushlogin"
-fi
 
 
 if [ "$config_environment" = "null" ]; then
